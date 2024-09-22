3e9d9b9787f405e13dab8c64163cf40de1295164
fizalangah <fizalangah2001@gmail.com>
2024-09-22 01:25:29 -0700

    simple Calculator

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,82 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
-export default function Home() {
+export default function Calculatecomponent() {
+  const [number1, setNumber1] = useState<number | null>(null);
+  const [number2, setNumber2] = useState<number | null>(null);
+  const [result, setResult] = useState<string>("");
+  
+
+const handelClick = (operation:string) =>{
+  if (number1 !== null && number2 !== null) {
+    let calcResult: number;
+    switch (operation) {
+      case "+":
+        calcResult = number1 + number2;
+        break;
+      case "-":
+        calcResult = number1 - number2;
+        break;
+      case "*":
+        calcResult = number1 * number2;
+        break;
+      case "/":
+        calcResult = number2 !== 0 ? number1 / number2 : NaN;
+        break;
+      default:
+        return;
+    }
+    setResult(calcResult.toString());
+  }
+    
+  }
+  const clearFields = () => {
+    setNumber1(null);
+    setNumber2(null);
+    setResult("");
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    
+   <div>
+    <div className='flex justify-center md:mt-[100px] mt-[30px] '><h1 className='text-white md:text-4xl text-2xl '>simple Calculator</h1></div>
+    <div className='flex justify-center items-center'>
+<div  className='bg-white h-[300px]  md:h-[400px] md:w-[400px] w-[300px] mt-[20px] mb-[50px] rounded-xl'>
+    <div className='flex justify-center items-center mt-[30px]'>
+      <div className="flex-col ml-[20px]">
+      <label className="ml-[30px] mb-[5px] font-semibold">Number1</label>
+    <input type="number"
+    value={number1 ?? ""}
+    onChange={(e) => setNumber1(Number(e.target.value))}
+     placeholder="enter number"
+     className='md:w-[150px] w-[100px] p-2 bg-white text-start text-sm font-medium text-black rounded-xl outline-none border-2  '/>
+      </div>
+      
+      <div className="flex-col">
+      <label className="ml-[30px] mb-[5px] font-semibold">Number1</label>
+    <input type="number"
+     value={number2 ?? ""}
+     onChange={(e) => setNumber2(Number(e.target.value))}
+     placeholder="enter number"
+     className='md:w-[150px] w-[100px] p-2 bg-white text-start text-sm font-medium text-black rounded-xl outline-none border-2  '/>
+      </div>
     </div>
-  );
+    <div className="ml-[30px] mt-[10px] text-2xl font-bold">
+    <button   onClick ={ () => handelClick("+")} className="p-4 bg-white rounded-xl ml-[10px] mt-[20px] w-[70px] border-2">+</button>
+    <button      onClick ={ () => handelClick("-")} className="p-4 bg-white rounded-xl ml-[10px] mt-[20px] w-[70px] border-2">-</button>
+    <button      onClick ={ () => handelClick("*")} className="p-4 bg-white rounded-xl ml-[10px] mt-[20px] w-[70px] border-2">*</button>
+    <button     onClick ={ () => handelClick("/")} className="p-4 bg-white  rounded-xl ml-[10px] mt-[20px] w-[70px] border-2">/</button>
+   </div>
+   <div className='flex justify-center items-center mt-[30px] flex-col'>
+   <label className=" mb-[5px] font-semibold">Result</label>
+    <input type="text"
+     value={result}
+     readOnly
+    placeholder=""
+     className='md:w-[350px] w-[250px] p-1 bg-white text-start text-xl font-medium text-black rounded-xl outline-none border-2  '/>
+    </div>
+      <div className="flex justify-center items-center mt-[30px]"><button  onClick={clearFields}  className="md:w-[350px] w-[250px] p-1   bg-white rounded-xl outline-none border-2  font-semibold text-xl ">clear</button></div>
+  </div>
+  </div>
+  </div>
+  )
 }
